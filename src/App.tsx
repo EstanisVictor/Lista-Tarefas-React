6268ec2625ccad06042a024ae28f34e05eb6c053
EstanisVictor <victorestanislau1@gmail.com>
2023-02-23 21:27:33 -0300

    newArea

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,45 @@ import * as c from './App.styles'
 import { useState } from 'react'
 import { Item } from './types/item'
 import { ListItem } from './components/ListItem/listItem';
+import { NewArea } from './components/NewArea/newArea';
 
 const App = () =>{
   
-  const [list, setList] = useState<Item[]>([
-    {id:1, 
-      description: 'Comprar Remedio', 
+  const [list, setList] = useState<Item[]>([]);
+
+  const handleAddTask = (taskName: string) =>{
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      description: taskName,
       done: false
-    },
-    {id:2, 
-      description: 'Estudar...', 
-      done: false
+    });
+    setList(newList);
+  }
+
+  const handleTaskChange = (id: number, done: boolean) => {
+    let newList = [...list];
+    for(let i in newList) {
+      if(newList[i].id === id) {
+        newList[i].done = done;
+      }
     }
-  ]);
+    setList(newList);
+  }
   
   return(
     <c.Container>
       <c.Area>
         <c.Header>Lista de Tarefas</c.Header>
+
+        <NewArea onEnter={handleAddTask} />
+
         {list.map((item, index)=>(
-          <ListItem key={index} item={item}/>
+          <ListItem 
+            key={index} 
+            item={item}
+            onChange={handleTaskChange}
+          />
         ))}
       </c.Area>
     </c.Container>
--- src/components/ListItem/listItem.tsx
+++ src/components/ListItem/listItem.tsx
@@ -1,14 +1,24 @@
 import * as C from './styles'
 import { Item } from '../../types/item';
+import { useState } from 'react';
 
 type Props = {
     item: Item;
+    onChange: (id: number, done: boolean) => void
 }
 
-export const ListItem = ({ item }: Props) => {
+export const ListItem = ({ item, onChange }: Props) => {
+
+    const [isChecked, setIsChecked] = useState(item.done);
+
     return(
-        <C.Container>
-            {item.description}
+        <C.Container done={isChecked}>
+            <input 
+                type = "checkbox" 
+                checked={isChecked}
+                onChange={e => onChange(item.id, e.target.checked)}
+            />
+            <label>{item.description}</label>
         </C.Container>
     );
 }
